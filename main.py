7ebfc91c08a24c8e6af228d0eb3eb2dd9dcc662a
Cross Kilt <45547810+KiltCross@users.noreply.github.com>
2022-01-20 23:22:41 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
 
 import matplotlib.image as mpimg
 from PIL import Image
 import matplotlib.pyplot as plt
-import matplotlib.animation as anim
 
 import numpy as np
 import math
@@ -82,9 +80,8 @@ def DS(desired_ds='train', batch_size=2*24, train_batch_size = 1, test_batch_siz
     #np.random.shuffle(ds)
 
   return ds
-
 def optimizer(grad, theta, first_iteration):
-  alpha = 0.09
+  alpha = 0.02
   beta_1 = 0.999
   beta_2 = 0.999						#initialize the values of the parameters
   epsilon = 1e-8
@@ -92,7 +89,7 @@ def optimizer(grad, theta, first_iteration):
     #return x*x -4*x + 4
   #def grad_func(x):					#calculates the gradient
     #return 2*x - 4
-  #theta_0 = 0	
+  #theta_0 = 0						#initialize the vector
   if first_iteration:					#initialize the vector
     global m_t
     global v_t
@@ -100,21 +97,15 @@ def optimizer(grad, theta, first_iteration):
     m_t = 0
     v_t = 0 
     t = 0
-		
+				#till it gets converged
   t+=1
   g_t = grad		#computes the gradient of the stochastic function
   m_t = beta_1*m_t+ (1-beta_1)*g_t	#updates the moving averages of the gradient
-  v_t = beta_2*v_t + (1-beta_2)*(g_t**2)	#updates the moving averages of the squared gradient
+  v_t = beta_2*v_t + (1-beta_2)*(g_t*g_t)	#updates the moving averages of the squared gradient
   m_cap = m_t/(1-(beta_1**t))		#calculates the bias-corrected estimates
   v_cap = v_t/(1-(beta_2**t))		#calculates the bias-corrected estimates
-  theta_prev = theta								
+  #theta_prev = theta								
   theta = theta - (alpha*m_cap)/((v_cap**(1/2))+epsilon)	#updates the parameters
-  #theta = theta - alpha*grad
-  #print((np.min(theta),np.min(grad)))
-  #print((np.sum(theta),np.sum(grad)))
-  #deri = alpha*grad
-  #print(theta[-1], grad[-1], deri[-1])
-
   return theta
 
 
@@ -124,14 +115,7 @@ def X_smoother_function(X, max_X, degree):
 def X_by_theta_unit_smoother_function(unit):
   if (unit > 1):
     #return 1
-    return unit
-    """
-    elif (unit < -1):
-      return -1
-    """
-  #elif (unit < 0):
-    #return 0
-
+     return unit
   else:
     return unit
 
@@ -139,24 +123,25 @@ def decompressor_2(compressed_image, image_shape, the_degree, give_decompress_im
   #print("decompressing the compressed image:") 
   if (len(compressed_image.shape) > 1): 
     compressed_image = compressed_image[0]
+  #print([compressed_image,image_shape])
+  #decompressed_image = tf.Variable(tf.zeros(image_shape))
   decompressed_image = np.zeros(image_shape)
   decompressed_image_original_sign = np.zeros(image_shape)
 
-  total_iteration = image_shape[0] * image_shape[1] * image_shape[2] * the_degree
   for length in range(image_shape[0]):
     for width in range(image_shape[1]):
       for color_channel in range(image_shape[2]-1):
         for degree in range(the_degree):
 
-            compressed_image[6*the_degree-1:] = degree
+            #start = time.time()
+            h_length = X_by_theta_unit_smoother_function(compressed_image[degree]* X_smoother_function(length+1, image_shape[0], degree+1))
+            h_length_r = X_by_theta_unit_smoother_function(compressed_image[(the_degree*3)+degree]* X_smoother_function(image_shape[0]-length, image_shape[0], degree+1))
+            #print([length,width,color_channel,degree,h_length_r])
+            h_width = X_by_theta_unit_smoother_function(compressed_image[the_degree+degree]* X_smoother_function(width+1, image_shape[1], degree+1))
+            h_width_r = X_by_theta_unit_smoother_function(compressed_image[(the_degree*3)+the_degree+degree]* X_smoother_function(image_shape[1]-width, image_shape[1], degree+1))
 
-            h_length = X_by_theta_unit_smoother_function(compressed_image[degree]* X_smoother_function(length+1, image_shape[0], compressed_image[6*the_degree+degree]))
-            h_length_r = X_by_theta_unit_smoother_function(compressed_image[(the_degree*3)+degree]* X_smoother_function(image_shape[0]-length, image_shape[0], compressed_image[9*the_degree+degree]))
-            h_width = X_by_theta_unit_smoother_function(compressed_image[the_degree+degree]* X_smoother_function(width+1, image_shape[1],compressed_image[7*the_degree+degree]))
-            h_width_r = X_by_theta_unit_smoother_function(compressed_image[(the_degree*3)+the_degree+degree]* X_smoother_function(image_shape[1]-width, image_shape[1], compressed_image[10*the_degree+degree]))
-
-            h_color_channel = X_by_theta_unit_smoother_function(compressed_image[(2*the_degree)+degree]*X_smoother_function(color_channel+1, image_shape[2], compressed_image[8*the_degree+degree]))
-            h_color_channel_r = X_by_theta_unit_smoother_function(compressed_image[(the_degree*3)+(2*the_degree)+degree]* X_smoother_function(image_shape[2]-color_channel, image_shape[2], compressed_image[11*the_degree+degree]))
+            h_color_channel = X_by_theta_unit_smoother_function(compressed_image[(2*the_degree)+degree]*X_smoother_function(color_channel+1, image_shape[2], degree+1))
+            h_color_channel_r = X_by_theta_unit_smoother_function(compressed_image[(the_degree*3)+(2*the_degree)+degree]* X_smoother_function(image_shape[2]-color_channel, image_shape[2], degree+1))
 
             decompressed_image[length,width,color_channel] += h_length + h_width + h_color_channel + h_length_r + h_width_r + h_color_channel_r
             del h_length
@@ -182,132 +167,36 @@ def loss_for_compressor_2(y_true, y_false):
 
   return loss
 
-def get_gradient_multiplier(y_true, y_pred, the_degree, exponent):
-  gradient = np.zeros(np.shape(exponent))
+def get_gradient(y_true, y_pred, the_degree):
+  #the_degree = int((size_of_theta-1)/3)
+  gradient = np.zeros(2*the_degree*3+1)
   matrix = -2*(y_true - y_pred)
-  #matrix  = 2*(y_true - y_true)
-  #print(np.sum(matrix))
   image_shape = np.array([np.shape(y_true)[0],np.shape(y_true)[1],np.shape(y_true)[2]])
   if (image_shape[2] == 4):
     image_shape[2] = 3
     matrix = matrix[:,:,:3]
-
+  
   for degree in range(the_degree):
-    exponent = (exponent * 0) + degree
+
     for lenght in range(image_shape[0]):
 
-      X_raised_to_exponent_theta = X_smoother_function(lenght+1, image_shape[0], exponent[degree])      
-      X_r_raised_to_exponent_theta = X_smoother_function(image_shape[0]-lenght, image_shape[0], exponent[(the_degree*3)+degree])
-
-      gradient[degree] += np.sum(matrix[lenght]*X_raised_to_exponent_theta)
-      gradient[(the_degree*3)+degree] += np.sum(matrix[lenght]*X_r_raised_to_exponent_theta)
+      gradient[degree] += np.sum(matrix[lenght]*X_smoother_function(lenght+1, image_shape[0], degree+1))
+      gradient[(the_degree*3)+degree] += np.sum(matrix[lenght]*X_smoother_function(image_shape[0]-lenght, image_shape[0], degree+1))
 
     for width in range(image_shape[1]):  
 
-      X_raised_to_exponent_theta = X_smoother_function(width+1, image_shape[1], exponent[the_degree+degree])
-      X_r_raised_to_exponent_theta = X_smoother_function(image_shape-width, image_shape[1], exponent[(the_degree*3)+the_degree+degree])
+      gradient[the_degree+degree] += np.sum(matrix[:,width]*X_smoother_function(width+1, image_shape[1], degree+1))
+      gradient[(the_degree*3)+the_degree+degree] += np.sum(matrix[:,width]*X_smoother_function(image_shape[1]-width, image_shape[1], degree+1))
 
-      gradient[the_degree+degree] += np.sum(matrix[:,width]*X_raised_to_exponent_theta)
-      gradient[(the_degree*3)+the_degree+degree] += np.sum(matrix[:,width]*X_r_raised_to_exponent_theta)
+    for color_channel in range(image_shape[2]): 
+ 
+      gradient[(2*the_degree)+degree] += np.sum(matrix[:,:,color_channel]*X_smoother_function(color_channel+1, image_shape[2], degree+1))
+      gradient[(the_degree*3)+(2*the_degree)+degree] += np.sum(matrix[:,:,color_channel]*X_smoother_function(image_shape[2]-color_channel, image_shape[2], degree+1))
 
-    for color_channel in range(image_shape[2]):  
+  gradient[-1] = np.sum(matrix)
+  gradient = np.divide(gradient,np.size(matrix))
+  gradient = np.divide(gradient,2*the_degree*3+1)
 
-      X_raised_to_exponent_theta = X_smoother_function(color_channel+1, image_shape[2], exponent[(2*the_degree)+degree])
-      X_r_raised_to_exponent_theta = X_smoother_function(image_shape[2]-color_channel, image_shape[2], exponent[(the_degree*3)+(2*the_degree)+degree])
-
-      gradient[(2*the_degree)+degree] += np.sum(matrix[:,:,color_channel]*X_raised_to_exponent_theta)
-      gradient[(the_degree*3)+(2*the_degree)+degree] += np.sum(matrix[:,:,color_channel]*X_r_raised_to_exponent_theta)
-
-    gradient = np.divide(gradient,np.size(matrix))
-    #gradient = np.divide(gradient,2*the_degree*3+1)
-    #print(np.sum(gradient))
-
-  return gradient
-
-def get_gradient_exponent(y_true, y_pred, the_degree, compressed_image):
-  max_gradient_value = 0.00000000001 #the maximal value what a gradint can be
-  gradient = np.zeros(int((np.size(compressed_image)-1)/2))
-  matrix = -2*(y_true - y_pred)
-  exponent=compressed_image[int((np.size(compressed_image)-1)/2):]
-  image_shape = np.array([np.shape(y_true)[0],np.shape(y_true)[1],np.shape(y_true)[2]])
-  if (image_shape[2] == 4):
-    image_shape[2] = 3
-    matrix = matrix[:,:,:3]
-
-  for degree in range(the_degree):
-    for lenght in range(image_shape[0]):
-
-      X_raised_to_exponent_theta = X_smoother_function(lenght+1, image_shape[0], exponent[degree])
-      ln_X = np.log((lenght+1)/image_shape[0])
-      multiplier_theta_for_X = compressed_image[degree]
-      
-      X_r_raised_to_exponent_theta = X_smoother_function(image_shape[0]-lenght, image_shape[0], exponent[(the_degree*3)+degree])
-      ln_X_r = np.log((image_shape[0]-lenght)/image_shape[0])
-      multiplier_theta_for_X_r = compressed_image[(the_degree*3)+degree]
-
-      gradient[degree] += np.sum(matrix[lenght] * X_raised_to_exponent_theta *ln_X * multiplier_theta_for_X)
-      gradient[(the_degree*3)+degree] += np.sum(matrix[lenght] * X_r_raised_to_exponent_theta * ln_X_r * multiplier_theta_for_X_r)
-
-    if (abs(gradient[degree]) > max_gradient_value):
-       gradient[degree] /= (abs(gradient[degree]) - max_gradient_value)
-
-    if (abs(gradient[(the_degree*3)+degree]) > max_gradient_value):
-       gradient[(the_degree*3)+degree] /= (abs(gradient[(the_degree*3)+degree]) - max_gradient_value)
-
-    for width in range(image_shape[1]):  
-
-      X_raised_to_exponent_theta = X_smoother_function(width+1, image_shape[1], exponent[the_degree+degree])
-      ln_X = np.log((width+1)/image_shape[1])
-      multiplier_theta_for_X = compressed_image[(the_degree*3)+the_degree+degree]
-
-      X_r_raised_to_exponent_theta = X_smoother_function(image_shape[1]-width, image_shape[1], exponent[(the_degree*3)+the_degree+degree])
-      ln_X_r = np.log((image_shape[1]-width)/image_shape[1])
-      multiplier_theta_for_X_r = compressed_image[(the_degree*3)+degree]
-
-      gradient[the_degree+degree] += np.sum(matrix[:,width]*X_raised_to_exponent_theta*ln_X*multiplier_theta_for_X)
-      gradient[(the_degree*3)+the_degree+degree] += np.sum(matrix[:,width]*X_r_raised_to_exponent_theta*ln_X_r*multiplier_theta_for_X_r)
-
-    if (abs(gradient[the_degree+degree]) > max_gradient_value):
-       gradient[the_degree+degree] /= (abs(gradient[the_degree+degree]) - max_gradient_value)
-
-    if (abs(gradient[(the_degree*3)+the_degree+degree]) > max_gradient_value):
-       gradient[(the_degree*3)+the_degree+degree] /= (abs(gradient[(the_degree*3)+the_degree+degree]) - max_gradient_value)
-
-    for color_channel in range(image_shape[2]):  
-
-      X_raised_to_exponent_theta = X_smoother_function(color_channel+1, image_shape[2], exponent[(2*the_degree)+degree])
-      ln_X = np.log((color_channel+1)/image_shape[2])
-      multiplier_theta_for_X = compressed_image[(2*the_degree)+degree]
-
-      X_r_raised_to_exponent_theta = X_smoother_function(image_shape[2]-color_channel, image_shape[2], exponent[(the_degree*3)+(2*the_degree)+degree])
-      ln_X_r = np.log((image_shape[2]-color_channel)/image_shape[2])
-      multiplier_theta_for_X_r = compressed_image[(the_degree*3)+(2*the_degree)+degree]
-
-      gradient[(2*the_degree)+degree] += np.sum(matrix[:,:,color_channel]*X_raised_to_exponent_theta*ln_X*multiplier_theta_for_X)
-      gradient[(the_degree*3)+(2*the_degree)+degree] += np.sum(matrix[:,:,color_channel]*X_r_raised_to_exponent_theta*ln_X_r*multiplier_theta_for_X_r)
-
-    if (abs(gradient[(2*the_degree)+degree]) > max_gradient_value):
-       gradient[(2*the_degree)+degree] /= (abs(gradient[(2*the_degree)+degree])-max_gradient_value)
-
-    if (abs(gradient[(the_degree*3)+(2*the_degree)+degree]) > max_gradient_value):
-       gradient[(the_degree*3)+(2*the_degree)+degree] /= (abs(gradient[(the_degree*3)+(2*the_degree)+degree]) - max_gradient_value)
-
-    gradient = np.divide(gradient,np.size(matrix))
-    #gradient = np.divide(gradient,(10-gradient)**2)
-    gradient = np.divide(gradient,2*the_degree*3+1)
-
-  return gradient
-
-def get_gradient_independent_term(y_true, y_pred, the_degree):
-  gradient = 0
-  matrix = -2*(y_true - y_pred)
-  matrix = matrix[:,:,:3]
-  #print(np.sum(matrix))
-  #matrix  = 2*(y_true - y_true)
-  gradient = np.sum(matrix)
-  gradient = gradient/np.size(matrix)
-  #gradient = gradient/(2*the_degree*3+1)
-  #print(gradient)
   return gradient
 
 def training_summary(epochs,actual_epoch,loss):
@@ -328,53 +217,49 @@ def training_summary(epochs,actual_epoch,loss):
   time_elapsed += (start - last_timing)
   time_left = (time_elapsed/Iteration)*((epochs)-Iteration)
 
-  size_of_output_to_be_delete = np.size(output_lines)
+  #size_of_output_to_be_delete = np.size(output_lines)
 
   #Adding the actual training summery line
   print("Epoch: "+str(actual_epoch+1)+"/"+str(epochs)+"  |  Iteration duration: "+str(start - last_timing)+" seconds  |  Time left: "+str(int(time_left/3512))+" hours - "+str(int((time_left%3512)/60))+" minutes - "+str(int(time_left%60))+" seconds  |  Time elapsed: "+str(int(time_elapsed/(60*60)))+" hours - "+str(int(time_elapsed%(60*60)/60))+" minutes - "+str(int(time_elapsed%60))+" seconds | Loss: "+str(loss[Iteration-1]))
-
+       
   last_timing = start
 
   #Printing a special end line (on the end of the loop)
-  if (actual_epoch+1 == epochs):
+  if (actual_epoch == epochs):
     print("Finish!, take: "+str(int(time_elapsed/(60*60)))+":"+str(int(time_elapsed%(60*60)/60))+":"+str(int(time_elapsed%60)))
     del last_timing 
     del time_elapsed 
     del Iteration 
     del output_lines
 
-def compressor_2(uncompress_image, compressed_image, the_degree, epochs=10, alpha=0.001, return_decompressed_image=True):
+def compressor_2(uncompress_image, compressed_image, the_degree,  epochs=10, alpha=0.001, return_decompressed_image=True):
   print("compressing image:")
   training_loss = np.zeros([epochs])
-  grad = np.zeros(np.shape(compressed_image))
 
   last_timing = time.time()
   time_elapsed = 0
   count = 1
   total_iteration = uncompress_image.shape[0] * uncompress_image.shape[1] * uncompress_image.shape[2]  * epochs
-
-  decompressed_image = decompressor_2(compressed_image,np.shape(uncompress_image), the_degree)
-  decompressed_image_to_save = Image.fromarray((abs(decompressed_image)*255).astype(np.uint8),"RGBA")
+  
+  decompressed_image  = decompressor_2(compressed_image,np.shape(uncompress_image), the_degree)
+  decompressed_image_to_save = Image.fromarray((decompressed_image*255).astype(np.uint8),"RGBA")
 
   path_progress_images = "Decompress_images_progress/"+str(0)+".png"
   decompressed_image_to_save.save(path_progress_images,format='png')
   for epoch in range(epochs):
 
     start = time.time()
-    
-    grad[:int((np.size(grad)-1)/2)] = get_gradient_multiplier(uncompress_image, decompressed_image, the_degree,compressed_image[:int((np.size(grad)-1)/2)],)
-    #grad[int((np.size(grad)-1)/2):-1] = get_gradient_exponent(uncompress_image, decompressed_image, the_degree,compressed_image)
-    grad[-1] = get_gradient_independent_term(uncompress_image, decompressed_image,the_degree)
-
+    grad = get_gradient(uncompress_image, decompressed_image,the_degree)
     compressed_image = optimizer(grad,compressed_image, epoch == 0)
-    #print(np.sum(compressed_image))
+    #print([epoch,compressed_image])
 
     training_loss[epoch] = loss_for_compressor_2(uncompress_image, decompressed_image)
 		    
     training_summary(epochs,epoch,training_loss)
 
     decompressed_image = decompressor_2(compressed_image,np.shape(uncompress_image), the_degree)
-    #decompressed_image_to_save = Image.fromarray((abs(decompressed_image)*255).astype(np.uint8),"RGBA")
+    #decompressed_image_to_save = Image.fromarray((decompressed_image*255).astype(np.uint8),"RGBA")
+    #path_progress_images = "/media/manuel/sda21/Proyctos/Hypercompresor/Decompress_images_progress/"+str(epoch)+".jpg"
     #path_progress_images = "Decompress_images_progress/"+str(epoch+1)+".png"
     #decompressed_image_to_save.save(path_progress_images,format='png')
 
@@ -383,37 +268,20 @@ def compressor_2(uncompress_image, compressed_image, the_degree, epochs=10, alph
   #if (return_decompressed_image) else compressed_image , training_loss
 
 #compressed_image = np.random.normal(size=[2*metadata("degree")*metadata('number_of_theta_of_same_X')*3+1])
-#compressed_image = np.random.normal(size=[2*2*metadata('degree')*3+1])
-#compressed_image = np.zeros([2*2*metadata("degree")*3+1])+0.00001
-#compressed_image = np.ones([2*2*metadata("degree")*3+1])
-compressed_image = np.load('compressed_image.npy')
-#compressed_image = np.load('compress_test_ds.npy')
-#compressed_image[int((np.size(compressed_image)-1)/2):-1] = np.ones(int((np.size(compressed_image)-1)/2))
-
-
+compressed_image = np.random.normal(size=[2*metadata('degree')*3+1])
+#compressed_image = np.zeros([2*metadata("degree")*3+1])+0.00001
 
 decompressed_image = decompressor_2(compressed_image,metadata("image_shape"),metadata("degree"))
 #plt.imshow(decompressed_image)
 
 train_ds=DS('train', train_batch_size=(metadata("train_batch_size")), mini_batch_size=metadata("mini_batch_size"))
-"""
-train_ds[0,:,:,0] = 0.8
-train_ds[0,:,:,1] = 0.00001
-train_ds[0,:,:,2] = 0.00001
-train_ds[0,:,:,3] = 1
-"""
-#compress_test_ds = np.load('compress_test_ds.npy')
 
-#train_ds[0] = decompressor_2(compress_test_ds,metadata("image_shape"),metadata("degree"))
+#train_ds[1] = 0.8
+#train_ds[1,:,:,3] = 1
 
-#uncompressed_image = Image.fromarray((train_ds[0]*255).astype(np.uint8),"RGBA")
-#uncompressed_image.save('uncompress_image.png',format='png')
+uncompressed_image = Image.fromarray((train_ds[0]*255).astype(np.uint8),"RGBA")
+uncompressed_image.save('uncompress_image.png',format='png')
 
-compressor_2(train_ds[0], compressed_image, metadata('degree'),  epochs=metadata('epoch'))
-"""
-for count, image in enumerate(decompressed_image):
-  decompressed_image_to_save = Image.fromarray((image*255).astype(np.uint8),"RGBA")
-  path_progress_images = "Decompress_images_progress/"+str(count+1)+".png"
-  decompressed_image_to_save.save(path_progress_images,format='png')
-"""
+compressor_2(train_ds[0], compressed_image, metadata('degree'), epochs=metadata('epoch'))
+
 
